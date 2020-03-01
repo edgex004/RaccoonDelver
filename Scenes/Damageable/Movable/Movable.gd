9e91a1c154f6f7ec5a0aadfcd32fe088eb7e8af0
David Von Edge <david.vonedge@smiths.com>
2020-02-29 19:52:42 -0800

    Fix errors from merge.

--- Scenes/Damageable/Movable/Movable.gd
+++ Scenes/Damageable/Movable/Movable.gd
@@ -27,17 +27,17 @@ func move_tile(dir_vector, movement_size):
 	var cur_pos = get_position()
 	var desired_tile_x = tile_x + dir_vector.x * movement_size
 	var desired_tile_y = tile_y + dir_vector.y * movement_size
-	var desired_pos = get_parent()._get_tile_pos(Vector2(desired_tile_x,desired_tile_y),get_parent().GroundTileMap)
+	var desired_pos = get_node('/root/Level')._get_tile_pos(Vector2(desired_tile_x,desired_tile_y),get_node('/root/Level').GroundTileMap)
 	if (!check_for_collision(desired_tile_x,desired_tile_y)):
 		#set_position(desired_pos)
 		move_tween.interpolate_property(self, "position", 
 		cur_pos, desired_pos, move_time, Tween.TRANS_SINE, Tween.EASE_IN)
 		move_tween.start()
-		get_parent().swap_tile(tile_x, tile_y, desired_tile_x,desired_tile_y)
+		get_node('/root/Level').swap_tile(tile_x, tile_y, desired_tile_x,desired_tile_y)
 
 func check_for_collision(x:int, y:int) -> bool:
 	# Returns true if the space cannot be moved to because something is there
-	var collider = get_parent().get_object(x,y)
+	var collider = get_node('/root/Level').get_object(x,y)
 	if (collider == null): return false
 	var space_state = get_world_2d().direct_space_state
 	if is_instance_valid(collider) and 'is_alive' in collider and collider.is_alive and collider.has_method('take_damage'):
