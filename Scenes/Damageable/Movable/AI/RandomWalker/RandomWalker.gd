115191a1ec4cfdc68f77671f5f56824ea427ec4e
Quinn Morrison <qamorrison@gmail.com>
2020-02-29 22:21:12 -0600

    Adding more art

--- Scenes/Damageable/Movable/AI/RandomWalker/RandomWalker.gd
+++ Scenes/Damageable/Movable/AI/RandomWalker/RandomWalker.gd
@@ -12,6 +12,14 @@ func _ready():
 	#screensize = get_viewport().get_size()
 	#set_position(screensize/2)
 	damage = 10
+	var rand_sprite = randi() % 3
+	match rand_sprite:
+		0:
+			get_node('Sprite').texture = preload('res://Scenes/Damageable/Movable/AI/RandomWalker/BasicEnemySkin.png')
+		1:
+			get_node('Sprite').texture = preload('res://Scenes/Damageable/Movable/AI/RandomWalker/Floater.png')
+		2:
+			get_node('Sprite').texture = preload('res://Scenes/Damageable/Movable/AI/RandomWalker/Fatty.png')
 
 
 func _on_Beat_timeout():
