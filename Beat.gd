2aefff022a86afe86e8cd6fa572fdf4df18c2761
David Von Edge <david.vonedge@smiths.com>
2020-02-29 17:01:33 -0800

    Adjust music pitch/speed in order to match the BPM. Add a tolerance at the end of the song so we don't sit waiting for music to restart.

--- Beat.gd
+++ Beat.gd
@@ -1,6 +1,8 @@
 extends Timer
 
-var BPM = 120
+var BPM = 100.0
+var MUSIC_BPM = 120.0
+var END_SONG_TOL_SEC = 2
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -9,10 +11,18 @@ var BPM = 120
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Music.set_pitch_scale(BPM/MUSIC_BPM)
+#	$Music.set_pitch_scale(3)
 	$Music.play()
+	connect("timeout", self,"on_beat_timeout")
+	wait_time = 60.0 / BPM
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func on_beat_timeout():
+	if($Music.stream.get_length() - $Music.get_playback_position() < END_SONG_TOL_SEC ):
+		$Music.play()
