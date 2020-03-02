74c670d3becffe0a1985641941c2557d1dc8a60c
David Von Edge <david.vonedge@smiths.com>
2020-03-01 21:19:36 -0800

    Forgot to add some keydrop svg files.

--- Scenes/Damageable/Movable/Player/Player.gd
+++ Scenes/Damageable/Movable/Player/Player.gd
@@ -74,7 +74,7 @@ func _process(delta):
 		if not is_first_player:
 			input_map = player2_inputs
 		for dir in input_map.keys():
-			if Input.is_action_just_pressed(dir):
+			if Input.is_action_pressed(dir):
 				move_tile(input_map[dir], 1)
 				has_moved = true
 
